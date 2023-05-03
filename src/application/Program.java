05b49844c5e5a0eb18258b032aab24ca91a9d130
Gabriel <ev04hs@gmail.com>
2023-05-02 21:43:10 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,5 +43,13 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),5000, department);
         sellerDao.insert( newSeller );
         System.out.println("Inserted! New id = "+ newSeller.getId());
+
+        System.out.println("=== TESTE 5: seller update ===");
+
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+
+        System.out.println("Update Completed");
     }
 }
